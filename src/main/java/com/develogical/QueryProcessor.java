abf7854697aebbea799e7a3d1be7c1bc0a3cc2a8
Katjana Kosic <katjana_kosic@yahoo.com>
2022-07-12 17:27:17 +0200

    name update

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -9,7 +9,7 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
         else if(query.toLowerCase().contains("name")){
-            return "Kati";
+            return "Katjana";
         }
         return "";
     }
--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -27,6 +27,6 @@ public class QueryProcessorTest {
 
     @Test
     public void isNotCaseSensitive2() throws Exception {
-        assertThat(queryProcessor.process("name"), containsString("Kati"));
+        assertThat(queryProcessor.process("name"), containsString("Katjana"));
     }
 }
